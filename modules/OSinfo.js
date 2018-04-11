21cabeb4e23ad8d2c8fc852988e0caaac5f17dae
Cienszki <wilq.wdz@gmail.com>
2018-04-11 16:59:36 +0200

    Fixed time

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -12,13 +12,12 @@ function getOSinfo() {
 	}
 	var release = os.release();
 	var cpu = os.cpus()[0].model;
-	var uptime = os.uptime();
 	var userInfo = os.userInfo();
 
 	console.log('System: ', type);
-	console.log('Release: ', release); // dlaczego nie console.log('Release: ' + release); ??
+	console.log('Release: ', release);
 	console.log('CPU model: ', cpu);
-	console.log('Uptime: ', time.print());	
+	console.log('Uptime: ', time.print);	
 	console.log('User name: ', userInfo.username);
 	console.log('Home dir:', userInfo.homedir);
 }
--- modules/time.js
+++ modules/time.js
@@ -1,12 +1,11 @@
 var os = require('os');
 var uptime = os.uptime();
 
-function convertTime() {
-	console.log(uptime + ' - wynik samego os,uptime()')
-	var hours = Math.floor(uptime / 3600),
-		uptime = uptime % 3600,
-		minutes = Math.floor(uptime / 60),
-		seconds = uptime % 60;
+function convertTime(time) {
+	var hours = Math.floor(time / 3600),
+		time = time % 3600,
+		minutes = Math.floor(time / 60),
+		seconds = time % 60;
 		return hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds ';
 }
-exports.print = convertTime;
+exports.print = convertTime(uptime);
